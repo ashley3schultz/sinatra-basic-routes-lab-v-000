d2aa8eb68129880d3261682253508c292f05bdbb
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-13 16:15:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,12 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Ashley"
   end
+
+  get '/hometown' do
+    "My hometown is Kalispell"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Oats In The Water"
+  end
 end
